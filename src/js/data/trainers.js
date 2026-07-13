d56da816afa446b3b7445a7e4f6061c546e44d67
jeremymarche123 <marchejeremy62160gren@gmail.com>
2026-07-13 18:29:56 +0200

    jsp

--- src/js/data/trainers.js
+++ src/js/data/trainers.js
@@ -1,18 +1,4 @@
 var TRAINERS = {
-    "Albert": `
-Pidgey
-Level: 12
-Ability: Keen Eye
-- Tackle
-- Gust
-
-Pidgeotto
-Level: 14
-Ability: Keen Eye
-- Tackle
-- Gust
-`,
-
     "Mortimer": `
 Gastly
 Level: 21
